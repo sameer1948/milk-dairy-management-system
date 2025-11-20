c28a11ccfe3152116e2723681cce3bfc69c5ed51
Sameer <sameer.sameer185@gmail.com>
2025-11-20 22:29:05 +0530

    Adding Spring Boot Config

---
+++ src/main/java/com/milk_dairy_management_system/Milk/Dairy/Management/MilkDairyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.milk_dairy_management_system.Milk.Dairy.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkDairyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkDairyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milk_dairy_management_system/Milk/Dairy/Management/MilkDairyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milk_dairy_management_system.Milk.Dairy.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkDairyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
